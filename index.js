9d47439226d02a803dca58c59edc91407a7d1eea
dennis-sketch <dennis.kimani2@student.moringaschool.comgit config --global user.name dennis-sketch>
2023-10-13 06:39:41 +0300

    Completed

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+const totalBatteries = batteryBatches.reduce((total, currentBatch) => total + currentBatch, 0);
+
+console.log(totalBatteries); // This will print the sum of all battery amounts
+
+
